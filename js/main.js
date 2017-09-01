570f8b966fb027454c84632b7b081096375835af
Sally Amkoa <sallyamkoa@gmail.com>
2017-08-31 20:53:42 -0500

    got buttons to work on website

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
-var currentPage = '#home'
-
-$('nav ul li').on('click',function(event){
-  if(!$(event.target).parent().hasClass('active')){
+$('nav ul li').on('click', function(event) {
+  // console.log("event target", event.target)
+  var currentNavLink =$("nav").find(".active")
     $(currentNavLink).removeClass('active')
-    currentNavLink = 
+  if(!$(event.target).parent().hasClass('active')){
+    var newNavLink = $(event.target).parent()
+    var currentPageName = $(currentNavLink).text().toLowerCase()
+    var currentPageId = "#" + currentPageName
+    // console.log('currentPageId', currentPageName)
+    var newPageName = $(event.target).text().toLowerCase()
+    var newPageId = "#" + newPageName
+    // console.log('newPageId', newPageId)
+
+
+
+    // console.log('newNavLink', newNavLink)
+    $(currentNavLink).removeClass('active')
+    $(newNavLink).addClass('active')
+    $(currentPageId).addClass('hidden')
+    $(newPageId).removeClass('hidden')
   }
 })
